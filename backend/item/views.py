4627d330a8d0614bfb7e71845981e3603b083070
Omar <omar1752003@gmail.com>
2025-03-16 18:02:48 +0200

    file formatted

--- backend/item/views.py
+++ backend/item/views.py
@@ -23,4 +23,3 @@ class ItemUploadView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
