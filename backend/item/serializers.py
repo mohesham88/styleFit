33bff8072f823fef64ec0e0be2111b6182ca7965
abdallhfattah <abdallh.fattah33@gmail.com>
2025-03-16 16:56:03 +0200

    update on create function

--- backend/item/serializers.py
+++ backend/item/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Item
-
 import cloudinary.uploader
 from rest_framework import serializers
 from .models import Item
@@ -8,23 +7,20 @@ from .models import Item
 
 class ItemSerializer(serializers.Serializer):
     id = serializers.CharField(read_only=True)
-    user = serializers.CharField(source='user.id', read_only=True)
+    user = serializers.CharField(source="user.id", read_only=True)
     image_url = serializers.CharField(read_only=True)  # Store Cloudinary URL
     image = serializers.ImageField(write_only=True)  # Only accept image input
 
-
     def create(self, validated_data):
-        image = validated_data.pop('image')
+        image = validated_data.pop("image")
 
         # Upload image to Cloudinary
         upload_result = cloudinary.uploader.upload(image)
-        image_url = upload_result['secure_url']
+        image_url = upload_result["secure_url"]
 
         # Create ClothingItem with default values
         clothing_item = Item.objects.create(
-            user=self.context['request'].user,
-            image_url=image_url
+            user=self.context["request"].user, image_url=image_url
         )
 
         return clothing_item
-
