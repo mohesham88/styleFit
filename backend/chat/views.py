81aac85b20aca4bcca836815c2b81ee196d579cc
mohamed hesham <scaret2tv@gmail.com>
2025-03-20 00:17:57 +0200

    using serpApi for buy suggestions

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+import user.views
 from item.models import Item
 from item.utils import get_genai_response
 
 from item.serializers import ItemSerializer
+from .serializers import OutfitSerializer
 from .sub_prompts import SUB_PROMPT_WITH_SUGGESTION, SUB_PROMPT_WITH_NO_SUGGESTION
 from utils.utils import extract_json_from_response
 
-from serpapi import GoogleSearch
+
 
 @api_view(["POST"])
 def chat(request):
     # user closet -> mongo db with
     closet = Item.objects.filter(user=request.user)
     serializer = ItemSerializer(closet, many=True)
-    context = serializer.data
+    context = [{"user_gender": request.user.gender}, serializer.data]
 
-    context[0]["user_gender"] = request.user.gender
 
     # message that is going to be sent to gemini (AI_MODEL)
     prompt = request.data.get("prompt")
@@ -31,11 +33,14 @@ def chat(request):
     else:
         prompt += SUB_PROMPT_WITH_NO_SUGGESTION
 
+
     # print("prompt: ", prompt)
     # print("context :", context)
 
     response = get_genai_response(prompt=prompt, context=str(context))
-
     outfits = extract_json_from_response(response.text)
+    print(outfits)
 
-    return Response(response.text, status=status.HTTP_200_OK)
+    serialized_outfits = OutfitSerializer(outfits, context={"user_gender" : request.user.gender}, many=True).data
+
+    return Response(serialized_outfits, status=status.HTTP_200_OK)
